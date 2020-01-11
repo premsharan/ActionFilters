a587e750b983c1ca481a05970da273fe3990cccb
verma <verma@ADMIN>
2020-01-12 02:02:01 +0530

    Add project files.

---
+++ ActionFilters/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionFilters.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Secure()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public ActionResult NonSecure()
+        {
+            return View();
+        }
+
+    }
+}
